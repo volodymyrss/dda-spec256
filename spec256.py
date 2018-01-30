6469b437a260e47d77d82bba6cfe0ef687fc41c1
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2018-01-30 09:55:43 +0100

    no pyfitds

--- spec256.py
+++ spec256.py
@@ -1,14 +1,16 @@
-from ddosa import *
+import os
+import astropy.io.fits as fits
+import ddosa
 
-class SpectraBins(DataAnalysis):
+class SpectraBins(ddosa.DataAnalysis):
     input_binsname="spectral_bins_256"
-    
+
     rmfbins=True
 
     version="v1"
     def main(self):
         self.binrmf=os.environ['CURRENT_IC']+"/ic/ibis/rsp/isgr_ebds_mod_0001.fits"
-        e=pyfits.open(self.binrmf)[1].data
+        e=fits.open(self.binrmf)[1].data
         self.bins=zip(e['E_MIN'],e['E_MAX'])
         self.binrmfext=self.binrmf+'[1]'
 
